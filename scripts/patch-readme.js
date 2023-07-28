9d117497e9bf1f308badb5d0470cd2b98e010102
Michael Bock <no-day@posteo.net>
2023-07-28 15:42:11 +0200

    Update extra_packages

--- scripts/patch-readme.js
+++ scripts/patch-readme.js
@@ -9,13 +9,36 @@ const patch = fs
   .readFileSync("./demo/src/Demo/Samples/MinimalComplete.purs", "utf8")
   .toString();
 
+const patchSection = (name, patch) => (source) => {
+  const regex = mkRegex(name);
+  const replace = mkReplace(name)(patch);
+
+  return source.replace(regex, replace);
+};
+
 const mkRegex = (name) =>
   new RegExp(`<!-- START ${name} -->[\\s\\S]*<!-- END ${name} -->`, "g");
+
 const mkReplace = (name) => (patch) =>
   `<!-- START ${name} -->\n${patch}\n<!-- END ${name} -->`;
 
 const codeBlock = (code) => `\`\`\`hs\n${code}\n\`\`\``;
 
-const result = source.replace(mkRegex("demo"), mkReplace("demo")(codeBlock(patch)));
+const patchAll = (patchData) => (source_) => {
+  let source = source_;
+  for (const [name, patch] of Object.entries(patchData)) {
+    source = patchSection(name, patch)(source);
+  }
+  return source;
+};
+
+//
+
+const patchData = {
+  demo: codeBlock(patch),
+  details: "foo",
+};
+
+const result = patchAll(patchData)(source);
 
 fs.writeFileSync("./README.md", result, "utf8");
