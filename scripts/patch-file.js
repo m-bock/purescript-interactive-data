1556e89de528bb5a202226c91c97e2d025153ed7
Michael Bock <no-day@posteo.net>
2023-09-04 08:33:49 +0200

    Add ts-check

--- scripts/patch-file.js
+++ scripts/patch-file.js
@@ -1,16 +1,36 @@
+// @ts-check
+
+/**
+ * @typedef {[RegExp, (match : string, groups: string[], index: number, all :string ) => string]} RegexPatch_
+ * @typedef {[RegExp, (match : string, ...args: any[] ) => string]} RegexPatch
+ */
+
 export const langs = {
   md: { open: "<!--", close: "-->" },
   purs: { open: "{-", close: "-}" },
 };
 
+/**
+ * @param {{open: string, close: string}} lang
+ * */
 const mkReplace =
   ({ open, close }) =>
+  /**
+   * @param {string} name
+   * @param {string} arg
+   */
   (name, arg) =>
+  /**
+   * @param {string} patch
+   */
   (patch) => {
     const arg_ = arg === "" ? "" : ` ${arg}`;
     return `${open} START ${name}${arg_} ${close}${patch}${open} END ${name} ${close}`;
   };
 
+/**
+ * @param {{open: string, close: string}} lang
+ * */
 const mkRegex = ({ open, close }) => {
   return new RegExp(
     `${open} START ([a-zA-Z0-9_]+)(.*?)${close}([\\s\\S]*?)${open} END ([a-zA-Z0-9_]+) ${close}`,
@@ -18,60 +38,72 @@ const mkRegex = ({ open, close }) => {
   );
 };
 
-export const patchAll = (lang) => (patchData) => (source) => {
-  const regex = mkRegex(lang);
+/**
+ * @param {object} lang
+ * @param {string} lang.open
+ * @param {string} lang.close
+ * */
+export const patchAll =
+  (lang) =>
+  /**
+   * @param {Object.<string, string | ((_1: string, _2: string, _3: number, _4: string) => string) | RegexPatch>} patchData
+   * */
+  (patchData) =>
+  /**
+   * @param {string} source
+   * */
+  (source) => {
+    const regex = mkRegex(lang);
 
-  let newSource = source;
+    let newSource = source;
 
-  newSource = source.replace(
-    regex,
-    (match, nameOpen, arg_, content, nameClose, index, all) => {
-      if (nameOpen !== nameClose) {
-        throw new Error(
-          `Mismatched open/close name: '${nameOpen}' is not '${nameClose}'`
-        );
+    newSource = source.replace(
+      regex,
+      (match, nameOpen, arg_, content, nameClose, index, all) => {
+        if (nameOpen !== nameClose) {
+          throw new Error(
+            `Mismatched open/close name: '${nameOpen}' is not '${nameClose}'`
+          );
+        }
+        const name = nameOpen;
+
+        const patch = patchData[name];
+
+        if (typeof patch === "undefined") {
+          throw new Error(`No patch for '${name}'`);
+        }
+
+        const arg = arg_.trim();
+
+        const newContent =
+          typeof patch === "function"
+            ? patch(content, arg, index, all)
+            : typeof patch === "string"
+            ? patch
+            : content;
+
+        return mkReplace(lang)(name, arg)(newContent);
       }
-      const name = nameOpen;
+    );
 
-      const patch = patchData[name];
+    const regexPatches = /** @type RegexPatch[] */ (
+      Object.values(patchData).filter((val) => Array.isArray(val))
+    );
 
-      if (typeof patch === "undefined") {
-        throw new Error(`No patch for '${name}'`);
-      }
+    for (const [regex, patch] of regexPatches) {
+      newSource = source.replace(regex, (...args) => {
+        const newContent =
+          typeof patch === "function"
+            ? patch(...args)
+            : typeof patch === "string"
+            ? patch
+            : (() => {
+                throw new Error("unsupported patch");
+              })();
 
-      const arg = arg_.trim();
-
-      const getNewContent = (patch_) =>
-        typeof patch_ === "function"
-          ? patch_(content, arg, index, all)
-          : typeof patch_ === "string"
-          ? patch
-          : content;
-
-      const newContent = getNewContent(patch);
-
-      return mkReplace(lang)(name, arg)(newContent);
+        return newContent;
+      });
     }
-  );
 
-  const regexPatches = Object.values(patchData).filter((val) =>
-    Array.isArray(val)
-  );
-
-  for (const [regex, patch] of regexPatches) {
-    newSource = source.replace(regex, (...args) => {
-      const newContent =
-        typeof patch === "function"
-          ? patch(...args)
-          : typeof patch_ === "string"
-          ? patch
-          : (() => {
-              throw new Error("unsupported patch");
-            })();
-
-      return newContent;
-    });
-  }
-
-  return newSource;
-};
+    return newSource;
+  };
